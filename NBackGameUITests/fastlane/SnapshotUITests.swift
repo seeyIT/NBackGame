718c220eb09820322f238eed2f4fe5c9e792837e
Kornel Miszczak <k.miszczak@2040.io>
2021-05-29 12:04:18 +0200

    - add fastlane for making screenshots - add FastlaneScreeshots Schema for creating screenshots and additional UITest class for it

---
+++ NBackGameUITests/fastlane/SnapshotUITests.swift
@@ -0,0 +1,45 @@
+//
+//  SnapshotUITests.swift
+//  NBackGameUITests
+//
+//  Created by Kornel Miszczak on 27/05/2021.
+//
+
+import XCTest
+
+class SnapshotUITests: XCTestCase {
+
+    func testMenuPlusTutorialSnapshots() throws {
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        Thread.sleep(forTimeInterval: TimeInterval(1))
+
+        snapshot("1MenuScreen")
+        Thread.sleep(forTimeInterval: TimeInterval(1))
+
+        app.buttons[AccessibilityIdentifier.Menu.tutorialButton].tap()
+        snapshot("2TutorialScreen")
+        Thread.sleep(forTimeInterval: TimeInterval(1))
+
+    }
+
+    func testGameSnapshots() throws {
+        let nBack = 2
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        
+        app.buttons[AccessibilityIdentifier.Menu.playButton].tap()
+        app.buttons["\(AccessibilityIdentifier.LevelSelection.levelPrefixButton)\(nBack)"].tap()
+        Thread.sleep(forTimeInterval: TimeInterval(10))
+        app.buttons[AccessibilityIdentifier.Game.positionButton].tap()
+        snapshot("2GameScreen")
+
+        Thread.sleep(forTimeInterval: TimeInterval(10))
+        app.buttons[AccessibilityIdentifier.Game.soundButton].tap()
+        snapshot("3GameScreen")
+        Thread.sleep(forTimeInterval: TimeInterval(1))
+
+    }
+}

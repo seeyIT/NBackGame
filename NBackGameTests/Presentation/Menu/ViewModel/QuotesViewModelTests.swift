479289f92aa34c1eb323ef9301d7e5838b4f1ef8
Kornel Miszczak <kornelmiszczak@gmail.com>
2022-06-22 18:04:26 +0200

    QuoteViewModel doesn't have any quote on start

--- NBackGameTests/Presentation/Menu/ViewModel/QuotesViewModelTests.swift
+++ NBackGameTests/Presentation/Menu/ViewModel/QuotesViewModelTests.swift
@@ -9,12 +9,15 @@ import XCTest
 
 class QuotesViewModel {
     
+    let quote: (author: String, content: String) = ("", "")
 }
 
 class QuotesViewModelTests: XCTestCase {
 
-    func test_init() {
+    func test_init_doesNotHaveAnyQuote() {
         let sut = QuotesViewModel()
-    }
 
+        XCTAssertEqual(sut.quote.author, "")
+        XCTAssertEqual(sut.quote.content, "")
+    }
 }

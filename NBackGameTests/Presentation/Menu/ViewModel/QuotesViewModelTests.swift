5df650707e6d45887cc8b26512fd67f2ddf655b6
Kornel Miszczak <kornelmiszczak@gmail.com>
2022-06-22 17:57:33 +0200

    Add QuotesViewModel Init test

---
+++ NBackGameTests/Presentation/Menu/ViewModel/QuotesViewModelTests.swift
@@ -0,0 +1,20 @@
+//
+//  QuotesViewModelTests.swift
+//  NBackGameTests
+//
+//  Created by Kornel Miszczak on 22/06/2022.
+//
+
+import XCTest
+
+class QuotesViewModel {
+    
+}
+
+class QuotesViewModelTests: XCTestCase {
+
+    func test_init() {
+        let sut = QuotesViewModel()
+    }
+
+}

62f26fce27d9225b83dca6ed8e3e3505ba2ac755
Kornel Miszczak <kornelmiszczak@gmail.com>
2023-04-20 15:51:18 +0200

    Enable PlayGameTests and run tests in dev

--- NBackGameUITests/PlayGameUITests.swift
+++ NBackGameUITests/PlayGameUITests.swift
@@ -1,203 +1,207 @@
-////
-////  TutorialUITests.swift
-////  NBackGameUITests
-////
-////  Created by Kornel Miszczak on 09/02/2021.
-////
 //
-//import XCTest
+//  TutorialUITests.swift
+//  NBackGameUITests
 //
-//class PlayGameUITests: XCTestCase {
-//    
-//    func test2nBack25RoundsSoundClicks() throws {
-//        let roundNumber = 25
-//        let nBack = 2
-//        
-//        let app = XCUIApplication()
+//  Created by Kornel Miszczak on 09/02/2021.
 //
-//        app.launch()
-//               
-//        app.buttons[AccessibilityIdentifier.Menu.playButton].tap()
-//        app.buttons["\(AccessibilityIdentifier.LevelSelection.levelPrefixButton)\(nBack)"].tap()
-//        
-//        let soundButton = app.buttons[AccessibilityIdentifier.Game.soundButton]
-//        _ = soundButton.waitForExistence(timeout: 5)
-//        
-//        soundButton.tap()
-//
-//        for _ in 1..<roundNumber {
-//            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: soundButton, handler: nil)
-//            waitForExpectations(timeout: 10.0, handler: nil)
-//            
-//            soundButton.tap()
-//        }
-//                
-//        let correctSelectionText = app.staticTexts[AccessibilityIdentifier.GameSummary.correctSelectionSoundText]
-//        _ = correctSelectionText.waitForExistence(timeout: 5)
-//        
-//        let correctSelectionTextLabel = correctSelectionText.label
-//        let incorrectSelectionSoundLabel = app.staticTexts[AccessibilityIdentifier.GameSummary.incorrectSelectionSoundText].label
-//      
-//        let value: Int = (Int(correctSelectionTextLabel) ?? 0) + (Int(incorrectSelectionSoundLabel) ?? 0)
-//        
-//        XCTAssertEqual(roundNumber - nBack, value)
-//        
-//        XCTAssertNotNil(correctSelectionTextLabel)
-//        XCTAssertNotNil(incorrectSelectionSoundLabel)
-//    }
-//    
-//    func test2nBack25RoundsPositionClicks() throws {
-//        let roundNumber = 25
-//        let nBack = 1
-//        
-//        let app = XCUIApplication()
-//
-//        app.launch()
-//               
-//        app.buttons[AccessibilityIdentifier.Menu.playButton].tap()
-//        app.buttons["\(AccessibilityIdentifier.LevelSelection.levelPrefixButton)\(nBack)"].tap()
-//        
-//        let positionButton = app.buttons[AccessibilityIdentifier.Game.positionButton]
-//        _ = positionButton.waitForExistence(timeout: 5)
-//        
-//        positionButton.tap()
-//
-//        for _ in 1..<roundNumber {
-//            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: positionButton, handler: nil)
-//            waitForExpectations(timeout: 10.0, handler: nil)
-//            
-//            positionButton.tap()
-//        }
-//                
-//        let correctSelectionText = app.staticTexts[AccessibilityIdentifier.GameSummary.correctSelectionPositionText]
-//        _ = correctSelectionText.waitForExistence(timeout: 5)
-//        
-//        let correctSelectionTextLabel = correctSelectionText.label
-//        XCTAssertNotNil(correctSelectionTextLabel)
-//
-//        let incorrectSelectionPositionLabel = app.staticTexts[AccessibilityIdentifier.GameSummary.incorrectSelectionPositionText].label
-//        XCTAssertNotNil(incorrectSelectionPositionLabel)
-//
-//        let value: Int = Int(correctSelectionTextLabel)! + Int(incorrectSelectionPositionLabel)!
-//        
-//        XCTAssertEqual(roundNumber - nBack, value)
-//        
-//    }
-//    
-//    func test2nBack25RoundsPositionAndSoundClicks() throws {
-//        let roundNumber = 25
-//        let nBack = 2
-//        
-//        let app = XCUIApplication()
-//
-//        app.launch()
-//               
-//        app.buttons[AccessibilityIdentifier.Menu.playButton].tap()
-//        app.buttons["\(AccessibilityIdentifier.LevelSelection.levelPrefixButton)\(nBack)"].tap()
-//        
-//        let positionButton = app.buttons[AccessibilityIdentifier.Game.positionButton]
-//        let soundButton = app.buttons[AccessibilityIdentifier.Game.soundButton]
-//        _ = positionButton.waitForExistence(timeout: 5)
-//        _ = soundButton.waitForExistence(timeout: 5)
-//
-//        positionButton.tap()
-//        soundButton.tap()
-//        
-//        for _ in 1..<roundNumber {
-//            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: positionButton, handler: nil)
-//            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: soundButton, handler: nil)
-//
-//            waitForExpectations(timeout: 10.0, handler: nil)
-//            
-//            positionButton.tap()
-//            soundButton.tap()
-//        }
-//                
-//        let correctSelectionPositionText = app.staticTexts[AccessibilityIdentifier.GameSummary.correctSelectionPositionText]
-//        let correctSelectionSoundText = app.staticTexts[AccessibilityIdentifier.GameSummary.correctSelectionSoundText]
-//        _ = correctSelectionPositionText.waitForExistence(timeout: 5)
-//        _ = correctSelectionSoundText.waitForExistence(timeout: 5)
-//
-//        let correctSelectionPositionTextLabel = correctSelectionPositionText.label
-//        let incorrectSelectionPositionLabel = app.staticTexts[AccessibilityIdentifier.GameSummary.incorrectSelectionPositionText].label
-//        let positionValue: Int = (Int(correctSelectionPositionTextLabel) ?? 0) + (Int(incorrectSelectionPositionLabel) ?? 0)
-//        
-//        XCTAssertEqual(roundNumber - nBack, positionValue)
-//        
-//        let correctSelectionSoundTextLabel = correctSelectionSoundText.label
-//        let incorrectSelectionSoundLabel = app.staticTexts[AccessibilityIdentifier.GameSummary.incorrectSelectionSoundText].label
-//        let soundValue: Int = (Int(correctSelectionSoundTextLabel) ?? 0) + (Int(incorrectSelectionSoundLabel) ?? 0)
-//        
-//        XCTAssertEqual(roundNumber - nBack, soundValue)
-//        
-//        XCTAssertNotNil(correctSelectionSoundTextLabel)
-//        XCTAssertNotNil(incorrectSelectionSoundLabel)
-//    }
-//    
-//    func testPositionClicksPlayTwiceAndBackToMenu() throws {
-//        let roundNumber = 25
-//        let nBack = 2
-//        
-//        let app = XCUIApplication()
-//
-//        app.launch()
-//               
-//        app.buttons[AccessibilityIdentifier.Menu.playButton].tap()
-//        app.buttons["\(AccessibilityIdentifier.LevelSelection.levelPrefixButton)\(nBack)"].tap()
-//        
-//        let positionButton = app.buttons[AccessibilityIdentifier.Game.positionButton]
-//        _ = positionButton.waitForExistence(timeout: 5)
-//        
-//        positionButton.tap()
-//
-//        for _ in 1..<roundNumber {
-//            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: positionButton, handler: nil)
-//            waitForExpectations(timeout: 10.0, handler: nil)
-//            positionButton.tap()
-//        }
-//                
-//        let playAgainButton = app.buttons[AccessibilityIdentifier.GameSummary.playAgainButton]
-//        _ = playAgainButton.waitForExistence(timeout: 5)
-//        playAgainButton.tap()
-//        
-//        _ = positionButton.waitForExistence(timeout: 5)
-//        positionButton.tap()
-//        for _ in 1..<roundNumber {
-//            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: positionButton, handler: nil)
-//            waitForExpectations(timeout: 10.0, handler: nil)
-//            positionButton.tap()
-//        }
-//        
-//        let menuButton = app.buttons[AccessibilityIdentifier.GameSummary.menuButton]
-//        _ = menuButton.waitForExistence(timeout: 5)
-//       
-//        menuButton.tap()
-//       
-//        let playButton = app.buttons[AccessibilityIdentifier.Menu.playButton]
-//        _ = playButton.waitForExistence(timeout: 5)
-//    }
-// 
-//    func testUnlockLevel3FromLevel2() throws {
-//        
-//    }
-//    
-//    func testBackToMenu() throws {
-//        let nBack = 2
-//        let app = XCUIApplication()
-//        
-//        app.launch()
-//        
-//        app.buttons[AccessibilityIdentifier.Menu.playButton].tap()
-//        app.buttons["\(AccessibilityIdentifier.LevelSelection.levelPrefixButton)\(nBack)"].tap()
-//        
-//        let soundButton = app.buttons[AccessibilityIdentifier.Game.soundButton]
-//        _ = soundButton.waitForExistence(timeout: 5)
-//        
-//        soundButton.tap()
-//        
-//        app.buttons[AccessibilityIdentifier.Game.showMenuButton].tap()
-//        
-//        let playButtonFromMenu = app.buttons[AccessibilityIdentifier.Menu.playButton]
-//        _ = playButtonFromMenu.waitForExistence(timeout: 3.0)
-//    }
-//}
+
+import XCTest
+
+class PlayGameUITests: XCTestCase {
+    
+    func test2nBack25RoundsSoundClicks() throws {
+        let roundNumber = 25
+        let nBack = 2
+        
+        let app = XCUIApplication()
+
+        app.launch()
+               
+        app.buttons[AccessibilityIdentifier.Menu.playButton].tap()
+        app.buttons["\(AccessibilityIdentifier.LevelSelection.levelPrefixButton)\(nBack)"].tap()
+        
+        let soundButton = app.buttons[AccessibilityIdentifier.Game.soundButton]
+        _ = soundButton.waitForExistence(timeout: 5)
+        
+        soundButton.tap()
+
+        for _ in 1..<roundNumber {
+            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: soundButton, handler: nil)
+            waitForExpectations(timeout: 10.0, handler: nil)
+            
+            soundButton.tap()
+        }
+                
+        let correctSelectionText = app.staticTexts[AccessibilityIdentifier.GameSummary.correctSelectionSoundText]
+        _ = correctSelectionText.waitForExistence(timeout: 5)
+        
+        let correctSelectionTextLabel = correctSelectionText.label
+        let incorrectSelectionSoundLabel = app.staticTexts[AccessibilityIdentifier.GameSummary.incorrectSelectionSoundText].label
+      
+        let value: Int = (Int(correctSelectionTextLabel) ?? 0) + (Int(incorrectSelectionSoundLabel) ?? 0)
+        
+        XCTAssertEqual(roundNumber - nBack, value)
+        
+        XCTAssertNotNil(correctSelectionTextLabel)
+        XCTAssertNotNil(incorrectSelectionSoundLabel)
+    }
+    
+    func test2nBack25RoundsPositionClicks() throws {
+        let roundNumber = 25
+        let nBack = 1
+        
+        let app = XCUIApplication()
+
+        app.launch()
+               
+        app.buttons[AccessibilityIdentifier.Menu.playButton].tap()
+        app.buttons["\(AccessibilityIdentifier.LevelSelection.levelPrefixButton)\(nBack)"].tap()
+        
+        let positionButton = app.buttons[AccessibilityIdentifier.Game.positionButton]
+        _ = positionButton.waitForExistence(timeout: 5)
+        
+        positionButton.tap()
+
+        for _ in 1..<roundNumber {
+            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: positionButton, handler: nil)
+            waitForExpectations(timeout: 10.0, handler: nil)
+            
+            positionButton.tap()
+        }
+                
+        let correctSelectionText = app.staticTexts[AccessibilityIdentifier.GameSummary.correctSelectionPositionText]
+        _ = correctSelectionText.waitForExistence(timeout: 5)
+        
+        let correctSelectionTextLabel = correctSelectionText.label
+        XCTAssertNotNil(correctSelectionTextLabel)
+
+        let incorrectSelectionPositionLabel = app.staticTexts[AccessibilityIdentifier.GameSummary.incorrectSelectionPositionText].label
+        XCTAssertNotNil(incorrectSelectionPositionLabel)
+
+        let value: Int = Int(correctSelectionTextLabel)! + Int(incorrectSelectionPositionLabel)!
+        
+        XCTAssertEqual(roundNumber - nBack, value)
+        
+    }
+    
+    func test2nBack25RoundsPositionAndSoundClicks() throws {
+        let roundNumber = 25
+        let nBack = 2
+        
+        let app = XCUIApplication()
+
+        app.launch()
+               
+        app.buttons[AccessibilityIdentifier.Menu.playButton].tap()
+        app.buttons["\(AccessibilityIdentifier.LevelSelection.levelPrefixButton)\(nBack)"].tap()
+        
+        let positionButton = app.buttons[AccessibilityIdentifier.Game.positionButton]
+        let soundButton = app.buttons[AccessibilityIdentifier.Game.soundButton]
+        _ = positionButton.waitForExistence(timeout: 5)
+        _ = soundButton.waitForExistence(timeout: 5)
+
+        positionButton.tap()
+        soundButton.tap()
+        
+        for _ in 1..<roundNumber {
+            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: positionButton, handler: nil)
+            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: soundButton, handler: nil)
+
+            waitForExpectations(timeout: 10.0, handler: nil)
+            
+            positionButton.tap()
+            soundButton.tap()
+        }
+                
+        let correctSelectionPositionText = app.staticTexts[AccessibilityIdentifier.GameSummary.correctSelectionPositionText]
+        let correctSelectionSoundText = app.staticTexts[AccessibilityIdentifier.GameSummary.correctSelectionSoundText]
+        _ = correctSelectionPositionText.waitForExistence(timeout: 5)
+        _ = correctSelectionSoundText.waitForExistence(timeout: 5)
+
+        let correctSelectionPositionTextLabel = correctSelectionPositionText.label
+        let incorrectSelectionPositionLabel = app.staticTexts[AccessibilityIdentifier.GameSummary.incorrectSelectionPositionText].label
+        let positionValue: Int = (Int(correctSelectionPositionTextLabel) ?? 0) + (Int(incorrectSelectionPositionLabel) ?? 0)
+        
+        XCTAssertEqual(roundNumber - nBack, positionValue)
+        
+        let correctSelectionSoundTextLabel = correctSelectionSoundText.label
+        let incorrectSelectionSoundLabel = app.staticTexts[AccessibilityIdentifier.GameSummary.incorrectSelectionSoundText].label
+        let soundValue: Int = (Int(correctSelectionSoundTextLabel) ?? 0) + (Int(incorrectSelectionSoundLabel) ?? 0)
+        
+        XCTAssertEqual(roundNumber - nBack, soundValue)
+        
+        XCTAssertNotNil(correctSelectionSoundTextLabel)
+        XCTAssertNotNil(incorrectSelectionSoundLabel)
+    }
+    
+    func testPositionClicksPlayTwiceAndBackToMenu() throws {
+        let roundNumber = 25
+        let nBack = 2
+        
+        let app = XCUIApplication()
+
+        app.launch()
+               
+        app.buttons[AccessibilityIdentifier.Menu.playButton].tap()
+        app.buttons["\(AccessibilityIdentifier.LevelSelection.levelPrefixButton)\(nBack)"].tap()
+        
+        let positionButton = app.buttons[AccessibilityIdentifier.Game.positionButton]
+        _ = positionButton.waitForExistence(timeout: 5)
+        
+        positionButton.tap()
+
+        for _ in 1..<roundNumber {
+            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: positionButton, handler: nil)
+            waitForExpectations(timeout: 10.0, handler: nil)
+            positionButton.tap()
+        }
+                
+        let playAgainButton = app.buttons[AccessibilityIdentifier.GameSummary.playAgainButton]
+        _ = playAgainButton.waitForExistence(timeout: 5)
+        playAgainButton.tap()
+        
+        _ = positionButton.waitForExistence(timeout: 5)
+        positionButton.tap()
+        for _ in 1..<roundNumber {
+            expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: positionButton, handler: nil)
+            waitForExpectations(timeout: 10.0, handler: nil)
+            positionButton.tap()
+        }
+        
+        let menuButton = app.buttons[AccessibilityIdentifier.GameSummary.menuButton]
+        _ = menuButton.waitForExistence(timeout: 5)
+       
+        menuButton.tap()
+       
+        let playButton = app.buttons[AccessibilityIdentifier.Menu.playButton]
+        _ = playButton.waitForExistence(timeout: 5)
+    }
+ 
+    func testUnlockLevel3FromLevel2() throws {
+        
+    }
+    
+    func testBackToMenu() throws {
+        let nBack = 2
+        let app = XCUIApplication()
+        
+        app.launch()
+        
+        app.buttons[AccessibilityIdentifier.Menu.playButton].tap()
+        app.buttons["\(AccessibilityIdentifier.LevelSelection.levelPrefixButton)\(nBack)"].tap()
+        
+        let soundButton = app.buttons[AccessibilityIdentifier.Game.soundButton]
+        _ = soundButton.waitForExistence(timeout: 5)
+        
+        soundButton.tap()
+        
+        let backButton = app.buttons[AccessibilityIdentifier.Game.showMenuButton]
+        _ = backButton.waitForExistence(timeout: 3.0)
+        backButton.tap()
+        
+        let playButtonFromMenu = app.buttons[AccessibilityIdentifier.Menu.playButton]
+        _ = playButtonFromMenu.waitForExistence(timeout: 3.0)
+    }
+}
+
+

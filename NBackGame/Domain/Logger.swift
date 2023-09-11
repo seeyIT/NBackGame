c66952beea31009bb05d29f5ec01b548adc58d5f
Kornel Miszczak <kornelmiszczak@gmail.com>
2023-09-11 15:21:13 +0200

    Remove firebase

--- NBackGame/Domain/Logger.swift
+++ NBackGame/Domain/Logger.swift
@@ -5,9 +5,11 @@
 //  Created by Kornel Miszczak on 28/08/2023.
 //
 
-import FirebaseAnalytics
-import Foundation
+//import FirebaseAnalytics
+//import Foundation
+//
+//func log(_ message: String, parameters: [String: Any]? = nil) {
+//    Analytics.logEvent(message, parameters: parameters)
+//}
 
-func log(_ message: String, parameters: [String: Any]? = nil) {
-    Analytics.logEvent(message, parameters: parameters)
-}
+func log(_: String) { }
